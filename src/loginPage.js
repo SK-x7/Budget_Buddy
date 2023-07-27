5e4d84ae9214bbf21236cdbd13f3fdc73cc4f0fe
SK-x7 <111245838+SK-x7@users.noreply.github.com>
2023-07-27 21:00:40 +0530

    mansiiiiiiii

--- src/loginPage.js
+++ src/loginPage.js
@@ -1,29 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.module.css";
+import styles from "./login.module.css";
 
 export default function LoginPage(params) {
   const navigate = useNavigate();
   return (
     <>
-      <div className="app">
-        <section className="sect1" style={{ fontSize: "2.5rem" }}>
-          <header className="headerr">
-            <h1 className="headerr1">keep saving</h1>
-            <h1 className="headerr2">keep growing</h1>
+      <div className={styles.app}>
+        <section className={styles.sect1} style={{ fontSize: "2.5rem" }}>
+          <header className={styles.headerr}>
+            <h1 className={styles.headerr1}>keep saving</h1>
+            <h1 className={styles.headerr2}>keep growing</h1>
           </header>
           <span>
             <img
               src="./resources/_0680d48e-fbbb-42d9-aafe-07870a57628c-removebg-preview - Copy.png"
               alt="Bitcoin"
-              id="image"
+              id={styles.image}
             />
           </span>
         </section>
-        <section className="sect2">
+        <section className={styles.sect2}>
           <span>
             <img
-              className="logo"
+              className={styles.logo}
               src="./resources/Group 3.png"
               alt="BudgetBuddy"
             />
@@ -37,10 +37,10 @@ export default function LoginPage(params) {
           {/* form */}
           <form action="#" style={{ display: "inline-block" }}>
             <span>Log in with</span>
-            <fieldset className="maang-btn">
-              <button className="fa fa-facebook" id="fb" />
-              <button className="fa fa-twitter" id="twitter" />
-              <button className="fa fa-google" id="google" />
+            <fieldset className={styles.maang_btn}>
+              <button className="fa fa-facebook" id={styles.fb} />
+              <button className="fa fa-twitter" id={styles.twitter} />
+              <button className="fa fa-google" id={styles.google} />
             </fieldset>
             <span>or</span>
             <fieldset>
@@ -49,14 +49,14 @@ export default function LoginPage(params) {
               <br />
               <input
                 type="password"
-                id="passwrdInputId"
+                id={styles.passwrdInputId}
                 placeholder="Password"
                 onkeyup="check()"
                 required
               />
               <br />
-              <p style={{ color: "red" }} id="errorMsgSpanId" />
-              <div className="pass">
+              <p style={{ color: "red" }} id={styles.errorMsgSpanId} />
+              <div className={styles.pass}>
                 <input type="checkbox" />
                 <label htmlFor>Keep me logged in</label>
                 <footer>
@@ -64,7 +64,7 @@ export default function LoginPage(params) {
                 </footer>
               </div>
               <span>
-                <button id="signin">Sign In</button>
+                <button id={styles.signin}>Sign In</button>
               </span>
             </fieldset>
             <span>or</span>
